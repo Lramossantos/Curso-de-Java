d96652d40a3d4d156f2399810140bea8d2cbabcf
Leonardo <leonardoramosdossantos92@gmail.com>
2025-01-23 18:55:59 -0300

    Add Java_01 and Java_02

---
+++ CursoJava/src/Java/Aula_02.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package Java;
+
+/**
+ * 
+ */
+public class Aula_02 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
