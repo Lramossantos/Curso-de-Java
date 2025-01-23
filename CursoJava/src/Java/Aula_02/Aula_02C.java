864d3dd80770eeab3ebb75393e8cb2c1d005dfa7
Leonardo <leonardoramosdossantos92@gmail.com>
2025-01-23 20:13:38 -0300

    Create 2 class for this commits in Aula_02 and edited file text

---
+++ CursoJava/src/Java/Aula_02/Aula_02C.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Java.Aula_02;
+
+/**
+ * 
+ */
+public class Aula_02C {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Leonardo Ramos \ndos Santos");
+
+	}
+
+}
---
+++ CursoJava/src/Java/Aula_02/Aula_02D.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Java.Aula_02;
+
+/**
+ * 
+ */
+public class Aula_02D {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("Leonardo Ramos dos Santos tem: %d anos de idade!", 20);
+
+	}
+
+}
