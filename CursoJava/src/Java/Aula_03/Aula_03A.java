086cfec570699ba4123c952c832109cf0f0ff06b
Leonardo <leonardoramosdossantos92@gmail.com>
2025-01-24 02:36:52 -0300

    Create class Aula_03A

---
+++ CursoJava/src/Java/Aula_03/Aula_03A.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package Java.Aula_03;
+
+/**
+ * 
+ */
+public class Aula_03A {
+	
+//Comandos de decisão em Java (IF, IF..ELSE, SWITCH e ?) - Curso de Java - Aula 03	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// == > < := <= != ! (true/false)
+		
+		int nota = 65;
+		int media = 60;
+		
+		if (nota >= 60) {
+			System.out.println("Aprovado!");
+		}
+		System.out.println("Fim do programa");
+
+	}
+
+}
