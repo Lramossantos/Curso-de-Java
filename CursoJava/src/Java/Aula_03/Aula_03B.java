793c388f8e7e08201e8860820777ae4899999b9f
Leonardo <leonardoramosdossantos92@gmail.com>
2025-01-24 02:37:16 -0300

    Create Aula_03B

---
+++ CursoJava/src/Java/Aula_03/Aula_03B.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package Java.Aula_03;
+
+/**
+ * 
+ */
+public class Aula_03B {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// == > < := <= != ! (true/false)
+		
+		int nota = 65;
+		int media = 60;
+		if (nota >= media) {
+			System.out.println("Aprovado");
+		}else {
+			System.out.println("Reprovado");
+		}
+		System.out.println("Fim do Programa");
+	}
+
+}
