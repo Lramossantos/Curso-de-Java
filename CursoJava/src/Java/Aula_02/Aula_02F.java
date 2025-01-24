349c9e3c2d39bffc27f45db0cfdb2e5e295ab032
Leonardo <leonardoramosdossantos92@gmail.com>
2025-01-24 01:41:13 -0300

    Create class Aula_02F

---
+++ CursoJava/src/Java/Aula_02/Aula_02F.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package Java.Aula_02;
+
+/**
+ * 
+ */
+public class Aula_02F {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int idade = 30;        // variável numérica inteira
+		double altura = 1.75;  // variável numérica de ponto flutuante
+		String nome = "João";  // variável de texto
+		boolean ativo = true;  // variável lógica
+		char letra = 'A';      // variável de caractere
+		
+		System.out.printf("Nome: %s, Idade: %d, Altura: %.2f, Letra: %c, Ativo: %b%n", 
+                nome, idade, altura, letra, ativo);		
+	}
+}
