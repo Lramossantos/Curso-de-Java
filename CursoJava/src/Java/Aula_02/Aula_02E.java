5b5d33684adfbfdcacb79c429b1d565034a65985
Leonardo <leonardoramosdossantos92@gmail.com>
2025-01-24 01:40:53 -0300

    Create class Aula_02E

---
+++ CursoJava/src/Java/Aula_02/Aula_02E.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package Java.Aula_02;
+
+/**
+ * 
+ */
+public class Aula_02E {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Variaveis Númericas		
+			//Inteiros:
+				int idade = 20;
+				byte b = 100;
+				short s = 1000;
+				long l = 100000;			
+			//Ponto Flutuante:
+				float peso = 78.8f;
+				double altura = 1.75;
+			
+		//Variáveis de Texto
+			//Strings:
+				String nome = "Leonardo Ramos";
+			
+		//Váriaveis Lógicas
+			//Booleana:
+				boolean ativo = true;
+		
+		//Variáveis de Caractere
+			//Char:
+				char letra = 'A';
+		
+		
+	}
+
+}
