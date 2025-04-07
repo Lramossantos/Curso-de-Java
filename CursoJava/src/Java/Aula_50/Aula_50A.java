094dea11a00181960d82c7582d468c70c37ddef6
leona <leona@LEO>
2025-04-07 01:45:32 -0300

    Create new Class

---
+++ CursoJava/src/Java/Aula_50/Aula_50A.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package Java.Aula_50;
+
+/**
+ * 
+ */
+public class Aula_50A {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
